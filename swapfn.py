a92b7a48746e9ab533886895aa75944473bdd083
guzzye13 <eddyguzman322@gmail.com>
2022-07-20 19:16:43 -0400

    Initial commit

---
+++ swapfn.py
@@ -0,0 +1,14 @@
+string1 = "Eddy is my first name."
+string2 = "Guzman is my last name."
+print("Before swapping:")
+print(string1)
+print(string2)
+
+temp = string1
+string1 = string2
+string2 = temp
+
+print("\nAfter swapping: ") # /n new line
+print(string1)
+print(string2)
+
